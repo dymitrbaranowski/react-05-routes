1bcbdc014d9461234bae111be29a6bfbfe0ea836
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2025-02-06 20:19:29 +0200

    fox2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,27 @@
 import { Routes, Route, NavLink } from 'react-router-dom';
-import Home from './Home';
-import Dogs from './Dogs';
-import Products from './Products';
-import NotFound from './/NotFound';
+import Home from '../pages/Home';
+import Dogs from '../pages/Dogs';
+import Products from '../pages/Products';
+import NotFound from '../pages/NotFound';
 
 export const App = () => {
   return (
     <div>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/dogs">Dogs</NavLink>
-        <NavLink to="/products">Products</NavLink>
-      </nav>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/dogs">Dogs</NavLink>
+        </li>
+        <li>
+          <NavLink to="/products">Products</NavLink>
+        </li>
+      </ul>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dogs" element={<Dogs />} />
-        <Route path="/products" element={<Products />} />
+        <Route path="/dogs/:dogId" element={<Products />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
