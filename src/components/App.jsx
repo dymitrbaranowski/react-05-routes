cf5306a95a9f4d9e37cb9cdbac6ce953cb9fc31b
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2025-02-06 11:08:25 +0200

    fox1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,20 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, NavLink } from 'react-router-dom';
 import Home from './Home';
-import About from './About';
+import Dogs from './Dogs';
 import Products from './Products';
 import NotFound from './/NotFound';
 
 export const App = () => {
   return (
     <div>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/dogs">Dogs</NavLink>
+        <NavLink to="/products">Products</NavLink>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/dogs" element={<Dogs />} />
         <Route path="/products" element={<Products />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
---
+++ src/components/Dogs.jsx
@@ -0,0 +1,10 @@
+const Dogs = () => {
+  return (
+    <div>
+      <h1>Dogs</h1>
+      <p>This is the Dogs page.</p>
+    </div>
+  );
+};
+
+export default Dogs;
