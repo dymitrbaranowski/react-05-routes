8e2318eeedb5b11e2c2a7350b64823c425df21d5
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2025-02-10 22:10:25 +0200

    fox2

--- src/pages/Dogs.jsx
+++ src/pages/Dogs.jsx
@@ -1,19 +1,32 @@
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
 const Dogs = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const dogId = searchParams.get('dogId');
   // useEffect(() => {
   //HTTP запрос, если нужно
   // }, []);
 
   return (
     <div>
-      {['dog-1', 'dog-2', 'dog-3', 'dog-4', 'dog-5'].map(dog => {
-        return (
-          <Link key={dog} to={`${dog}`}>
-            {dog}
-          </Link>
-        );
-      })}
+      <input
+        type="text"
+        placeholder="Search"
+        value={dogId}
+        onChange={evt => setSearchParams({ dogId: evt.target.value })}
+      />
+      <button onClick={() => setSearchParams({ c: 'hello' })}>change sp</button>
+      <ul>
+        {['dog-1', 'dog-2', 'dog-3', 'dog-4', 'dog-5'].map(dog => {
+          return (
+            <li key={dog}>
+              <Link key={dog} to={`${dog}`}>
+                {dog}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
