87560d459abcfc695a45ac5b613e40b95cd6d300
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2025-02-17 11:51:16 +0200

    fox1

--- src/pages/Dogs.jsx
+++ src/pages/Dogs.jsx
@@ -3,13 +3,7 @@ import React, { useState } from 'react';
 import { Link, useLocation, useSearchParams } from 'react-router-dom';
 
 const Dogs = () => {
-  const [dogs, setDogs] = useState([
-    'dog-1',
-    'dog-2',
-    'dog-3',
-    'dog-4',
-    'dog-5',
-  ]);
+  const [dogs] = useState(['dog-1', 'dog-2', 'dog-3', 'dog-4', 'dog-5']);
   const location = useLocation();
   const [searchParams, setSearchParams] = useSearchParams();
   const dogId = searchParams.get('dogId') ?? '';
